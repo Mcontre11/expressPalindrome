1661fa8875d4fa19fc4c15391ad11baf8b6b0385
Resilient Coders <resilientcoders@admins-MacBook-Pro.local>
2021-10-27 16:51:23 -0400

    commit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
+
+
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
